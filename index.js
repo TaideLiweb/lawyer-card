62b22b85e5cd82708573e5ba3caa99caf3d734e6
TaideLiweb <teddy55987@gmail.com>
2023-05-03 00:49:34 +0800

    完成律師端個人頁面

--- index.js
+++ index.js
@@ -1,136 +1,143 @@
-const express = require('express')
-const engine = require('ejs-locals')
+const express = require('express');
+const engine = require('ejs-locals');
 
-const app = express()
+const app = express();
 
-app.engine('ejs', engine) // 設定ejs為樣版引擎
-app.set('views', './views') // 設定讀取的資料夾為根目錄的views資料夾
-app.set('view engine', 'ejs')
+app.engine('ejs', engine); // 設定ejs為樣版引擎
+app.set('views', './views'); // 設定讀取的資料夾為根目錄的views資料夾
+app.set('view engine', 'ejs');
 
 //導入css資料夾裡的東西
-app.use('/css', express.static('css'))
+app.use('/css', express.static('css'));
 
 //導入靜態檔案資料夾
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // 首頁
 app.get('/', function (req, res) {
-    res.render('index', {
-        lawyerSide: false,
-        clientSide: true,
-        conCall: false,
-    })
-})
+  res.render('index', {
+    lawyerSide: false,
+    clientSide: true,
+    conCall: false,
+  });
+});
 // 律師端首頁
-const lawyer = express.Router()
+const lawyer = express.Router();
 
 lawyer.get('/', (req, res) => {
-    res.render('index', {
-        lawyerSide: true,
-        clientSide: false,
-        conCall: false,
-    })
-})
-app.use('/lawyer', lawyer)
+  res.render('index', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
+app.use('/lawyer', lawyer);
 
 // 視訊頁
 app.get('/con-call', (req, res) => {
-    res.render('con-call', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: true,
-    })
-})
+  res.render('con-call', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: true,
+  });
+});
 
 // 律卡部落 start
 
-const lawyerCardBlog = express.Router()
+const lawyerCardBlog = express.Router();
 
 lawyerCardBlog.get('/', (req, res) => {
-    res.render('lawyerCardBlog/index', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/index', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/brand-board', (req, res) => {
-    res.render('lawyerCardBlog/brand-board', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/brand-board', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/new-post', (req, res) => {
-    res.render('lawyerCardBlog/new-post', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/new-post', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/lawer-board', (req, res) => {
-    res.render('lawyerCardBlog/lawer-board', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/lawer-board', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/law-knowledge', (req, res) => {
-    res.render('lawyerCardBlog/law-knowledge', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/law-knowledge', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/hot-topic', (req, res) => {
-    res.render('lawyerCardBlog/hot-topic', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('lawyerCardBlog/hot-topic', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
 lawyerCardBlog.get('/post', (req, res) => {
-    res.render('lawyerCardBlog/post', {
-        lawyerSide: false,
-        clientSide: false,
-        conCall: false,
-    })
-})
-app.use('/lawyerCardBlog', lawyerCardBlog)
+  res.render('lawyerCardBlog/post', {
+    lawyerSide: false,
+    clientSide: false,
+    conCall: false,
+  });
+});
+app.use('/lawyerCardBlog', lawyerCardBlog);
 
 // 律卡部落 end
 
 // 個人頁面 start
-const personPage = express.Router()
+const personPage = express.Router();
 
 personPage.get('/', (req, res) => {
-    res.render('personPage/index', {
-        lawyerSide: true,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('personPage/index', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
 personPage.get('/preview', (req, res) => {
-    res.render('personPage/preview', {
-        lawyerSide: true,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('personPage/preview', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
 personPage.get('/my-post', (req, res) => {
-    res.render('personPage/my-post', {
-        lawyerSide: true,
-        clientSide: false,
-        conCall: false,
-    })
-})
+  res.render('personPage/my-post', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
 personPage.get('/consult-record', (req, res) => {
-    res.render('personPage/consult-record', {
-        lawyerSide: true,
-        clientSide: false,
-        conCall: false,
-    })
-})
-app.use('/personPage', personPage)
+  res.render('personPage/consult-record', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
+personPage.get('/change-password', (req, res) => {
+  res.render('personPage/change-password', {
+    lawyerSide: true,
+    clientSide: false,
+    conCall: false,
+  });
+});
+app.use('/personPage', personPage);
 // 個人頁面 end
-const PORT = 3000
-app.listen(PORT, () => console.log(`server is now listening on port ${PORT}`))
+const PORT = 3000;
+app.listen(PORT, () => console.log(`server is now listening on port ${PORT}`));
